da5f406c46a3a854f17c0a394f5ca338831de503
SwZap <schenad4@gmail.com>
2022-09-01 17:13:16 -0500

    Add files via upload

---
+++ Results/Cv Calculation/Brsa6e-4-71_95-4/g_r.m
@@ -0,0 +1,36 @@
+%
+clc
+clear
+close all
+
+Num_image=10;
+for fi=1:Num_image
+    I=imread([num2str(fi),'.png']);
+    thresh=graythresh(I);
+    I1=~im2bw(I,thresh);
+    I1=bwareaopen(I1,3);
+    % Ibw = imfill(I1,'holes');
+    Ilabel = bwlabel(I1);
+    figure
+    imagesc(I1);title('input figure');
+    % %step 1:Do distance statistics
+    stat = regionprops(Ilabel,'centroid');
+    num_islands=max(max(Ilabel));
+    dis=zeros(length(num_islands),2);
+    for x = 1: numel(stat)
+        dis(x,:)=[stat(x).Centroid(1),stat(x).Centroid(2)];
+    end
+    [g_r{fi},distri{fi},x]=pair_function_dis(dis,size(Ilabel,1),76);
+    %
+   
+end
+
+Gt_r=0;
+for i=1:Num_image
+    Gt_r=Gt_r+g_r{i};
+end
+Gt_r=Gt_r/Num_image;
+figure
+plot(x,Gt_r,'rd');
+print('-djpeg','G(r)')
+close all;
---
+++ Results/Cv Calculation/Brsa6e-4-71_95-4/num2ima4.m
@@ -0,0 +1,95 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%             Make Image from "*.txt" files
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc;clear;
+
+siz=100;
+        
+MX=[1:siz*4];                                                      %x-axis
+MY=[3^(1/2):3^(1/2):siz*3^(1/2)];                                %y-axis
+
+interval=1;
+finaldata=10;
+
+%read the .txt from the folder
+load(['Br.txt']);
+
+for i=0:interval:finaldata-1;
+str=['X',num2str(i)];
+eval([(str),'=Br(siz*i+1:siz*i+siz,:)']);
+end
+
+clc;
+
+%change .txt into image and save as .jpg
+for i=0:interval:finaldata-1;
+str=['X',num2str(i)];    
+[m,n]=size(eval(str));                                           %calculate the size of matrix
+figure
+hold on
+markersizeA=round(siz*sqrt(120/(m*n)));
+markersizeB=round(siz*sqrt(100/(m*n)));                            %background
+
+%**************************************************************************
+
+[q,p]=find(eval(str)==1);
+pp=p*2+q-2;                                                        %function to calculate relationship
+ppp=pp+siz*2;
+ppp=ppp-siz*4*(ppp>siz*4);
+plot(pp,q,'.','Color',[254/255,254/255,254/255],...        %'.' means dot, 'color' define gray color
+                  'markersize',markersizeB)                        %define the dot size
+plot(ppp,q,'.','Color',[254/255,254/255,254/255],...        %'.' means dot, 'color' define gray color
+                  'markersize',markersizeB)
+              
+                    
+[q,p]=find(eval(str)==1);
+qq=q+siz;
+pp=p*2+q-2+siz;                                                        %function to calculate relationship
+ppp=pp+siz*2;
+ppp=ppp-siz*4*(ppp>siz*4);
+plot(pp,qq,'.','Color',[254/255,254/255,254/255],...        %'.' means dot, 'color' define gray color
+                  'markersize',markersizeB)                        %define the dot size
+plot(ppp,qq,'.','Color',[254/255,254/255,254/255],...        %'.' means dot, 'color' define gray color
+                  'markersize',markersizeB)
+
+              
+[q,p]=find(eval(str)==0);
+pp=p*2+q-2;
+ppp=pp+siz*2;
+ppp=ppp-siz*4*(ppp>siz*4);
+plot(pp,q,'.','Color',[0/255,0/255,0/255],...         
+                  'markersize',markersizeA)
+plot(ppp,q,'.','Color',[0/255,0/255,0/255],...         
+                  'markersize',markersizeA)  
+              
+              
+[q,p]=find(eval(str)==0);
+qq=q+siz;
+pp=p*2+q-2+siz;
+ppp=pp+siz*2;
+ppp=ppp-siz*4*(ppp>siz*4);
+plot(pp,qq,'.','Color',[0/255,0/255,0/255],...         
+                  'markersize',markersizeA)
+plot(ppp,qq,'.','Color',[0/255,0/255,0/255],...         
+                  'markersize',markersizeA)              
+              
+              
+              
+%**************************************************************************
+
+axis([0 347*siz/100 0 siz*2])                                       %make sure the white are almost same
+
+axis off;
+
+set (gcf,'Position',[150,50,siz*5.5,siz*5.5]);                    %300,100 is the ordination; 1000,600 is the height and width for window   
+
+set(gca,'LooseInset',get(gca,'TightInset'));                 %Tight in the page
+
+set (gcf,'PaperPositionMode','auto');                       %for save, otherwise save image will change the size
+
+set (gca,'position',[0,0,1,1]);
+
+print('-dpng',num2str(i+1));
+
+close all;
+end
